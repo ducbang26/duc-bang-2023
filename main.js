f06805c22cefe5b41ef699f77c1ce6107ee963fd
iceTgenerative <135957914+iceTgenerative@users.noreply.github.com>
2023-12-28 15:21:49 +0700

    custom cursor + fix global font size

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ import "./styles/index.scss";
 import { gsap } from "gsap";
 import Lenis from "@studio-freight/lenis";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Tween } from "gsap/gsap-core";
 
 document.querySelector("#app").innerHTML = `
   
-<div class="heading" style="height: auto">
+<div class="heading home-header" style="height: auto">
   <div id="hero-caption"
     class="content-full-width text-align-center parallax-scroll-caption subtitle-padding-left no-padding-bottom">
     <div class="inner">
@@ -20,6 +19,67 @@ document.querySelector("#app").innerHTML = `
         <span>perfect blend of creativity</span>
         <span>and functionality</span>
       </div>
+    </div>
+  </div>
+</div>
+
+<div class="custom-cursor no-select cursor-init pressed">
+  <div class="overlay cursor-tiles-inner">
+    <span>
+      <div class="rotate-cursor">Scroll <svg height="47"
+          viewBox="0 0 64 47" width="64" xmlns="http://www.w3.org/2000/svg">
+          <g fill-rule="evenodd" transform="translate(.321569 .725586)">
+            <path
+              d="m36.8278451 45.6240234c.970052 0 9.7773437-7.6040039 26.421875-22.8120117l-16.8408204-22.8120117 3.5699849 22.8120117c-9.7374118 15.2080078-14.1210916 22.8120117-13.1510395 22.8120117z">
+            </path>
+            <path d="m56.1767578 21.3847656v2.0036621l-56.17341143 1.9235835-.00334637-7.0173335z"
+              fill-rule="nonzero"></path>
+          </g>
+        </svg></div>
+    </span>
+  </div>
+  <div class="overlay cursor-work-inner">
+    <span>
+      <div class="rotate-cursor" style="transform: rotate(1.51582e-13deg);">View <svg height="47" viewBox="0 0 64 47"
+          width="64" xmlns="http://www.w3.org/2000/svg">
+          <g fill-rule="evenodd" transform="translate(.321569 .725586)">
+            <path
+              d="m36.8278451 45.6240234c.970052 0 9.7773437-7.6040039 26.421875-22.8120117l-16.8408204-22.8120117 3.5699849 22.8120117c-9.7374118 15.2080078-14.1210916 22.8120117-13.1510395 22.8120117z">
+            </path>
+            <path d="m56.1767578 21.3847656v2.0036621l-56.17341143 1.9235835-.00334637-7.0173335z"
+              fill-rule="nonzero"></path>
+          </g>
+        </svg></div>
+    </span>
+  </div>
+  <div class="overlay cursor-lab-inner">
+    <span>
+      <div class="rotate-cursor" style="transform: rotate(1.51582e-13deg);">Archive <svg height="47"
+          viewBox="0 0 64 47" width="64" xmlns="http://www.w3.org/2000/svg">
+          <g fill-rule="evenodd" transform="translate(.321569 .725586)">
+            <path
+              d="m36.8278451 45.6240234c.970052 0 9.7773437-7.6040039 26.421875-22.8120117l-16.8408204-22.8120117 3.5699849 22.8120117c-9.7374118 15.2080078-14.1210916 22.8120117-13.1510395 22.8120117z">
+            </path>
+            <path d="m56.1767578 21.3847656v2.0036621l-56.17341143 1.9235835-.00334637-7.0173335z"
+              fill-rule="nonzero"></path>
+          </g>
+        </svg></div>
+    </span>
+  </div>
+  <div class="overlay cursor-job-inner">
+    <span>
+      <div class="rotate-cursor" style="transform: rotate(1.51582e-13deg);">More info</div>
+    </span>
+  </div>
+  <div class="custom-cursor-inner custom-cursor-span-video">
+    <div class="play"><svg width="13" height="15" viewBox="0 0 13 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 7.5V0L6.5 3.75L13 7.5L6.5 11.25L0 15V7.5Z" fill="#1C1D23"></path>
+      </svg>
+    </div>
+    <div class="pause"><svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="2" width="3" height="15" fill="#1C1D23"></rect>
+        <rect x="9" width="3" height="15" fill="#1C1D23"></rect>
+      </svg>
     </div>
   </div>
 </div>
@@ -45,7 +105,7 @@ document.querySelector("#app").innerHTML = `
       <div class="project-box-number">
         01
       </div>
-      <div cursor-class="selected" class="project-box-image" >
+      <div  class="project-box-image" >
         <a href="/brener-21-boutique-project" class="link w-inline-block"><img
             src="https://assets-global.website-files.com/656f349790ab66e73956b52d/656f349790ab66e73956b55c_brener-clip-min.jpg"
             loading="lazy" sizes="(max-width: 991px) 80vw, 33vw"
@@ -72,7 +132,7 @@ document.querySelector("#app").innerHTML = `
       <div class="project-box-number">
         02
       </div>
-      <div cursor-class="selected" class="project-box-image" >
+      <div  class="project-box-image" >
         <a href="/brener-21-boutique-project" class="link w-inline-block"><img
           src="https://assets-global.website-files.com/656f349790ab66e73956b52d/656f349790ab66e73956b558_hashuftim-thumb-min.jpg"
           loading="lazy" sizes="(max-width: 991px) 80vw, 33vw"
@@ -99,7 +159,7 @@ document.querySelector("#app").innerHTML = `
       <div class="project-box-number">
         03
       </div>
-      <div cursor-class="selected" class="project-box-image" >
+      <div  class="project-box-image" >
         <a href="/brener-21-boutique-project" class="link w-inline-block"><img
           src="https://assets-global.website-files.com/656f349790ab66e73956b52d/656f349790ab66e73956b55b_gordosky-thumb-min.jpg"
           loading="lazy" sizes="(max-width: 991px) 80vw, 33vw"
@@ -126,7 +186,7 @@ document.querySelector("#app").innerHTML = `
       <div class="project-box-number">
         04
       </div>
-      <div cursor-class="selected" class="project-box-image" >
+      <div  class="project-box-image" >
         <a href="/brener-21-boutique-project" class="link w-inline-block"><img
           src="https://assets-global.website-files.com/656f349790ab66e73956b52d/656f349790ab66e73956b557_tsharnihovsky-thumb-min.jpg"
           loading="lazy" sizes="(max-width: 991px) 80vw, 33vw"
@@ -202,6 +262,8 @@ document.querySelector("#app").innerHTML = `
 <div style="height: 100vh"></div>
 `;
 
+gsap.registerPlugin(ScrollTrigger);
+
 const images = document.querySelectorAll("img");
 let imagesIndex = 0;
 
@@ -232,144 +294,307 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-gsap.registerPlugin(ScrollTrigger);
+initScript();
 
-const tl = gsap.timeline();
+function initScript() {
+  pageTransition();
+  initMarqueeAnimation();
+  initStickyCursorWithDelay();
+}
 
-tl.from(".caption-timeline span", {
-  duration: 1,
-  y: "100%",
-  opacity: 0,
-  ease: "power3.out",
-  delay: 0.7,
-  stagger: {
-    amount: 0.3,
-  },
-}).from(".video-wrapper", {
-  duration: 1,
-  y: "5%",
-  opacity: 0,
-  ease: "power3.out",
-  stagger: {
-    amount: 0.3,
-  },
-});
+function pageTransition() {
+  var tl = gsap.timeline();
 
-let clipValue = {
-  one: 10,
-  two: 10,
-  three: 50,
-};
-
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".video-wrapper",
-      start: `center center`,
-      end: "+=700",
-      pin: true,
-      scrub: true,
-    },
-  })
-  .to(
-    clipValue,
-    {
-      duration: 1,
-      one: 0,
-      two: 0,
-      three: 0,
-      onUpdate: () => {
-        gsap.set(".hero__video", {
-          clipPath: `inset(${clipValue.one}% ${clipValue.two}% round ${clipValue.three}px)`,
-        });
-      },
-    },
-    "<"
-  );
-
-gsap.to(".headline-content", {
-  transform: 'translate(0px, 0px)',
-  ease: "power1.inOut",
-  duration: .7,
-  opacity: 1,
-  scrollTrigger: {
-    trigger: ".headline-content",
-    start: "center bottom",
-    end: "center 10%",
-    toggleActions: "play reverse play reverse"
-  }
-});
-
-const projectBoxNumber = gsap.utils.toArray(".project-box-number");
-
-projectBoxNumber.forEach((item) => {
-  gsap.to(item, {
-    translate: "0% -30%",
-    ease: "power2.inOut",
+  tl.from(".caption-timeline span", {
     duration: 1,
-    scrollTrigger: {
-      trigger: item,
-      start: "top bottom",
-      end: "bottom top",
-      scrub: 1,
+    y: "100%",
+    opacity: 0,
+    ease: "power3.out",
+    delay: 0.7,
+    stagger: {
+      amount: 0.3,
+    },
+  }).from(".video-wrapper", {
+    duration: 1,
+    y: "5%",
+    opacity: 0,
+    ease: "power3.out",
+    stagger: {
+      amount: 0.3,
     },
   });
-});
 
-const projectBoxImage = gsap.utils.toArray(".project-box-image");
+  let clipValue = {
+    one: 10,
+    two: 10,
+    three: 50,
+  };
 
-projectBoxImage.forEach((item) => {
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".video-wrapper",
+        start: `center center`,
+        end: "+=700",
+        pin: true,
+        scrub: true,
+      },
+    })
+    .to(
+      clipValue,
+      {
+        duration: 1,
+        one: 0,
+        two: 0,
+        three: 0,
+        onUpdate: () => {
+          gsap.set(".hero__video", {
+            clipPath: `inset(${clipValue.one}% ${clipValue.two}% round ${clipValue.three}px)`,
+          });
+        },
+      },
+      "<"
+    );
 
-  gsap.to(item, {
-    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-    ease: "power4.out",
-    duration: 1.5,
+  gsap.to(".headline-content", {
+    transform: "translate(0px, 0px)",
+    ease: "power1.inOut",
+    duration: 0.7,
+    opacity: 1,
     scrollTrigger: {
-      trigger: item,
-      start: "top 80%",
-      end: "bottom center",
+      trigger: ".headline-content",
+      start: "center bottom",
+      end: "center 10%",
+      toggleActions: "play reverse play reverse",
     },
-  }, '-=1');
+  });
 
-});
+  const projectBoxNumber = gsap.utils.toArray(".project-box-number");
+
+  projectBoxNumber.forEach((item) => {
+    gsap.to(item, {
+      translate: "0% -30%",
+      ease: "power2.inOut",
+      duration: 1,
+      scrollTrigger: {
+        trigger: item,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 1,
+      },
+    });
+  });
+
+  const projectBoxImage = gsap.utils.toArray(".project-box-image");
+
+  projectBoxImage.forEach((item) => {
+    gsap.to(
+      item,
+      {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        ease: "power4.out",
+        duration: 1.5,
+        scrollTrigger: {
+          trigger: item,
+          start: "top 80%",
+          end: "bottom center",
+        },
+      },
+      "-=1"
+    );
+  });
+}
 
 //marquee section
+function initMarqueeAnimation() {
+  let currentScroll = 0;
+  let isScrollingDown = true;
+  let arrow = document.querySelectorAll(".arrow");
 
-let currentScroll = 0;
-let isScrollingDown = true;
-let arrow = document.querySelectorAll(".arrow");
+  let tween = gsap
+    .to(".marquee-part", {
+      xPercent: -100,
+      repeat: -1,
+      duration: 5,
+      ease: "linear",
+    })
+    .totalProgress(0.5);
 
-let tween = gsap.to(".marquee-part", {
-xPercent: -100,
-repeat: -1,
-duration: 5,
-ease: "linear",
-})
-.totalProgress(0.5);
+  gsap.set(".marquee-inner", { xPercent: -50 });
 
-gsap.set(".marquee-inner", { xPercent: -50 });
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > currentScroll) {
+      isScrollingDown = true;
+    } else {
+      isScrollingDown = false;
+    }
 
-window.addEventListener("scroll", () => {
-  if(window.scrollY > currentScroll) {
-    isScrollingDown = true;
-  } else {
-    isScrollingDown = false;
+    gsap.to(tween, {
+      timeScale: isScrollingDown ? 1 : -1,
+    });
+
+    arrow.forEach((item) => {
+      if (isScrollingDown) {
+        item.classList.remove("active");
+      } else {
+        item.classList.add("active");
+      }
+    });
+
+    currentScroll = window.scrollY;
+  });
+}
+
+function initStickyCursorWithDelay() {
+  // Sticky Cursor with delay
+  // https://greensock.com/forums/topic/21161-animated-mouse-cursor/
+
+  var posXBtn = 0;
+  var posYBtn = 0;
+  var posXImage = 0;
+  var posYImage = 0;
+  var mouseX = 0;
+  var mouseY = 0;
+
+  if (document.querySelector(".custom-cursor, .mouse-pos-list-image")) {
+    gsap.to({}, 0.0083333333, {
+      repeat: -1,
+      onRepeat: function () {
+        if (document.querySelector(".custom-cursor")) {
+          posXBtn += (mouseX - posXBtn) / 6;
+          posYBtn += (mouseY - posYBtn) / 6;
+          gsap.set($(".custom-cursor"), {
+            css: {
+              left: posXBtn,
+              top: posYBtn,
+            },
+          });
+          gsap.set($(".custom-cursor .rotate-cursor"), {
+            css: {
+              rotate: (mouseX - posXBtn) / 3,
+            },
+          });
+        }
+        if (document.querySelector(".mouse-pos-list-image")) {
+          posXImage += (mouseX / 4 - posXImage) / 6;
+          posYImage += (mouseY - posYImage) / 6;
+          gsap.set($(".mouse-pos-list-image"), {
+            css: {
+              left: posXImage,
+              top: posYImage,
+            },
+          });
+        }
+      },
+    });
   }
 
-  gsap.to(tween, {
-    timeScale: isScrollingDown ? 1 : -1,
+  $(document).on("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
   });
 
-  arrow.forEach((item) => {
-    if (isScrollingDown) {
-      item.classList.remove('active');
+  // Mouse Init
+  $("main").on("mousemove", function () {
+    if ($(".custom-cursor").hasClass("cursor-init")) {
     } else {
-      item.classList.add('active');
+      $(".custom-cursor").addClass("cursor-init");
     }
-  })
+  });
 
-  currentScroll = window.scrollY;
-});
+  // Link Hover
+  $("a").on("mouseenter", function () {
+    $(".custom-cursor").addClass("cursor-hover");
+  });
+  $("a").on("mouseleave", function () {
+    $(".custom-cursor").removeClass("cursor-hover");
+  });
 
+  // Pressed
+  $("main").on("mousedown", function () {
+    $(".custom-cursor").addClass("pressed");
+  });
+  $("main").on("mouseup", function () {
+    $(".custom-cursor").removeClass("pressed");
+  });
 
+  // Work Case Hover
+  $(".project-box").on("mouseenter", function () {
+    $(".custom-cursor").addClass("cursor-work");
+  });
+  $(".project-box").on("mouseleave", function () {
+    $(".custom-cursor").removeClass("cursor-work");
+  });
 
+  // Archive Preview Hover
+  $(".lab-preview").on("mouseenter", function () {
+    $(".custom-cursor").addClass("cursor-lab");
+  });
+  $(".lab-preview").on("mouseleave", function () {
+    $(".custom-cursor").removeClass("cursor-lab");
+  });
+
+  // // Jobs Preview Hover
+  // $('.single-job').on('mouseenter', function() {
+  //   $('.custom-cursor').addClass('cursor-job');
+  // });
+  // $('.single-job').on('mouseleave', function() {
+  //   $('.custom-cursor').removeClass('cursor-job');
+  // });
+  // $('.single-job .btn-click').on('mouseenter', function() {
+  //   $('.custom-cursor').addClass('cursor-job-tiny');
+  // });
+  // $('.single-job .btn-click').on('mouseleave', function() {
+  //   $('.custom-cursor').removeClass('cursor-job-tiny');
+  // });
+
+  // Home Header Rotate
+  $(".home-header").on("mousemove", function () {
+    if ($(".custom-cursor").hasClass("cursor-tiles")) {
+    } else {
+      $(".custom-cursor").addClass("cursor-tiles");
+    }
+  });
+  $(".home-header").on("mouseleave", function () {
+    $(".custom-cursor").removeClass("cursor-tiles");
+  });
+
+  // Service + Collective Image
+  $(".mouse-pos-list-image-hover").on("mouseenter", function () {
+    $(".mouse-pos-list-image").addClass("active");
+  });
+  $(".mouse-pos-list-image-hover").on("mouseleave", function () {
+    $(".mouse-pos-list-image").removeClass("active");
+  });
+
+  $(".mouse-pos-list-image-ul li").on("mouseenter mouseleave", function () {
+    var index = $(this).index();
+    $(".mouse-pos-list-image-ul, .mouse-pos-list-image").each(function () {
+      $("li", this).eq(index).siblings().removeClass("active");
+      $("li", this).eq(index).addClass("active");
+    });
+  });
+
+  $(".collective-ul li").on("mouseenter", function () {
+    $(".fixed-cursor-wrap").addClass("inactive");
+  });
+  $(".collective-ul li").on("mouseleave", function () {
+    $(".fixed-cursor-wrap").removeClass("inactive");
+  });
+
+  // Single Vimeo
+  $(".video-hover").on("mouseenter", function () {
+    $(".custom-cursor").addClass("cursor-video");
+  });
+  $(".video-hover").on("mouseleave", function () {
+    $(".custom-cursor").removeClass("cursor-video");
+  });
+  $(".video-hover .vimeo-control-play").on("mouseenter", function () {
+    $(".custom-cursor").addClass("cursor-video-play");
+    $(".custom-cursor").removeClass("cursor-video-pause");
+  });
+  $(".video-hover .vimeo-control-pause").on("mouseenter", function () {
+    $(".custom-cursor").addClass("cursor-video-pause");
+    $(".custom-cursor").removeClass("cursor-video-play");
+  });
+}
